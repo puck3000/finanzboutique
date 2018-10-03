20f5824c8ee2f1425fed2d9480cd1935dde6d7ae
Daniel Lerch <daniel@lerchpanther.ch>
2018-10-03 23:45:36 +0200

    done

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,189 @@
 <template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        finanzboutiqueSite
-      </h1>
-      <h2 class="subtitle">
-        Online Visitenkarte für Finanzboutique
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+  <div id="mainWrap">
+    <section v-if="page1" :class="'color' + randomNumber()" id="firstCont" @click="toggleSections">
+      <div class="centerBoth">
+        <h1>David Kobel</h1>
+        <h2>Finanzboutique</h2>
       </div>
-    </div>
   </section>
+  <section v-else id="secondCont" :class="'bgc' + currentNumber" class="bgc5" @click="toggleSections">
+    <div class="centerBoth">
+      <ul>
+        <li>
+          <h2>Vermögen</h2>
+        </li>
+        <li>
+          <h2>Steuern</h2>
+        </li>
+        <li>
+          <h2>Vorsorge</h2>
+        </li>
+      </ul>
+      <article id="adresse">
+        <a href="#" >Steinhofstrasse 29, 3400 Burgdorf</a>
+        <a href="tel:+41765277840">076 527 78 40</a>
+        <a href="mailto:dk@davidkobel.ch">dk@davidkobel.ch</a>
+      </article>
+    </div>
+    
+  </section>
+  
+
+  </div>
+  
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
+  data() {
+    return {
+      page1: true,
+      currentNumber: 1,
+      newNumber: 2
+    }
+  },
+  methods: {
+    // Math.floor(Math.random() * 4) + 1
+    randomNumber() {
+      let newNumber
+      do {
+        newNumber = Math.floor(Math.random() * 4) + 1
+      } while (this.currentNumber === newNumber)
+      this.newNumber = newNumber
+      return newNumber
+    },
+
+    toggleSections: function() {
+      this.page1 = !this.page1
+      this.currentNumber = this.newNumber
+    }
   }
+
+  //return Math.floor(Math.random() * 4) + 1
 }
 </script>
 
 <style>
-
 /* Sample `apply` at-rules with Tailwind CSS
 .container {
   @apply min-h-screen flex justify-center items-center text-center mx-auto;
 }
 */
 
+/*
+/--------------------
+/ layout
+/--------------------
+*/
 
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+section {
+  width: 100vw;
+  height: 100vh;
+  display: grid;
+  color: white;
+}
+
+.centerBoth {
+  margin: auto;
   text-align: center;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+ul {
+  text-decoration: none;
+  list-style: none;
+  padding-left: 0;
+}
+
+article {
+  margin-top: 2em;
+}
+
+/*
+/--------------------
+/ colors
+/--------------------
+*/
+
+.bgc1 {
+  color: #6290e7;
+}
+
+.bgc2 {
+  color: #ff6f4a;
+}
+
+.bgc3 {
+  color: #a66f8d;
+}
+
+.bgc4 {
+  color: #00af7c;
+}
+
+.bgc5 {
+  background-color: #ffffff;
+}
+
+.color1 {
+  background-color: #6290e7;
+}
+
+.color2 {
+  background-color: #ff6f4a;
+}
+
+.color3 {
+  background-color: #a66f8d;
+}
+
+.color4 {
+  background-color: #00af7c;
+}
+
+/*
+/--------------------
+/ fonts
+/--------------------
+*/
+
+@font-face {
+  font-family: 'Jost*';
+  src: url('~/fonts/Jost/OpenType/Jost-400-Book.otf');
+  font-style: normal;
+  font-weight: normal;
+}
+
+@font-face {
+  font-family: 'Jost*';
+  src: url('~/fonts/Jost/OpenType/Jost-700-Bold.otf');
+  font-style: bold;
+  font-weight: bold;
+}
+
+body {
+  font-family: 'Jost*', Futura, 'Trebuchet MS', Arial;
+}
+
+h1 {
+  font-size: 3em;
+  font-weight: bold;
+  text-transform: uppercase;
+}
+h2 {
+  font-size: 2em;
+  font-weight: normal;
+}
+
+#secondCont h2 {
+  font-size: 3em;
+  font-weight: bold;
+  text-transform: uppercase;
+}
+
+article a {
   display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  color: inherit;
+  text-decoration: none;
+  font-size: 1.5em;
 }
 </style>
